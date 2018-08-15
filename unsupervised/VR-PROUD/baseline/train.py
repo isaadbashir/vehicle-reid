91d9929d8d036716c71da8f2bc0e6355fdfaf6ec
saad.bashir <saad.bashir@hotmail,com>
2018-08-15 19:38:30 +0500

    Added the code for converting the dataset to hdf5 format. This will help in tackling the memory issues.
    As this is the initial test for the memroy related issues
    
    - Still need to refine the dataset-conversion code for general purposes
    - Improve the code quality

--- unsupervised/VR-PROUD/baseline/train.py
+++ unsupervised/VR-PROUD/baseline/train.py
@@ -1,28 +1,21 @@
 from __future__ import division, print_function, absolute_import
 
-import os
-import numpy as np
-
 import tensorflow as tf
-
+from keras import backend as K
 from keras.applications.resnet50 import ResNet50
 from keras.backend.tensorflow_backend import set_session
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import SGD
-from keras.preprocessing import image
-from keras.applications.resnet50 import preprocess_input
-from keras.utils.np_utils import to_categorical
-from keras.optimizers import SGD
-from keras.layers import Input
-from keras.layers import Dense, Flatten, Dropout
 from keras.initializers import RandomNormal
+from keras.layers import Dense, Flatten, Dropout
+from keras.layers import Input
 from keras.models import Model
-from keras import backend as K
-from keras.models import load_model
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils.io_utils import HDF5Matrix
 
-DATASET = '../dataset/veri'
-LIST = os.path.join(DATASET, 'train.list')
-TRAIN = os.path.join(DATASET, 'bounding_box_train')
+hdf5_path = '../dataset/VeRi/hdf5/veri-dataset.hdf5'  # address to where you want to save the hdf5 file
+
+images = HDF5Matrix(hdf5_path, 'train_img')
+labels = HDF5Matrix(hdf5_path, 'train_labels')
 
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
@@ -36,28 +29,11 @@ base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(
 x = base_model.output
 x = Flatten(name='flatten')(x)
 x = Dropout(0.5)(x)
-x = Dense(702, activation='softmax', name='fc8', kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(x)
+x = Dense(770, activation='softmax', name='fc8', kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(x)
 net = Model(input=base_model.input, output=x)
 
 for layer in net.layers:
    layer.trainable = True
-
-# load data
-images, labels = [], []
-with open(LIST, 'r') as f:
-  for line in f:
-    line = line.strip()
-    img, lbl = line.split()
-    img = image.load_img(os.path.join(TRAIN, img), target_size=[224, 224])
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img) 
-
-    images.append(img[0])
-    labels.append(int(lbl))
-
-images = np.array(images)
-labels = to_categorical(labels)
 
 # train
 batch_size = 16
---
+++ unsupervised/VR-PROUD/util/dataset-converter.py
@@ -0,0 +1,91 @@
+import glob
+import os
+
+import h5py
+import numpy as np
+from keras.applications.resnet50 import preprocess_input
+from keras.preprocessing import image
+from keras.utils.np_utils import to_categorical
+
+hdf5_path = '../dataset/VeRi/hdf5/veri-dataset.hdf5'  # address to where you want to save the hdf5 file
+veri_dataset_path = 'D:/VeRi/image_train/'
+veri_data_file = '../dataset/VeRi/name_train.txt'
+total_labels = 770
+
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end='\r')
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
+
+# read addresses and labels from the 'train' folder
+addrs = glob.glob(os.path.join(veri_dataset_path, '*.jpg'))
+
+data_order = 'tf'  # 'th' for Theano, 'tf' for Tensorflow
+
+# check the order of data and chose proper data shape to save images
+if data_order == 'th':
+    train_shape = (len(addrs), 3, 224, 224)
+elif data_order == 'tf':
+    train_shape = (len(addrs), 224, 224, 3)
+
+# open a hdf5 file and create earrays
+hdf5_file = h5py.File(hdf5_path, mode='w')
+
+hdf5_file.create_dataset("train_img", train_shape, np.int8)
+hdf5_file.create_dataset("train_labels", (len(addrs) + 1, total_labels), np.int8)
+
+printProgressBar(0, len(addrs), prefix='Progress:', suffix='Complete', length=50)
+
+i = 0
+# load data
+
+label = []
+with open(veri_data_file, 'r') as f:
+    for line in f:
+        line = line.strip()
+
+        img = line
+        lbl = int(img.split("_")[0])
+
+        imagePath = os.path.join(veri_dataset_path, img)
+
+        img = image.load_img(imagePath, target_size=[224, 224])
+        img = image.img_to_array(img)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+
+        img = np.array(img)
+
+        # if the data order is Theano, axis orders should change
+        if data_order == 'th':
+            img = np.rollaxis(img, 2)
+        # save the image and calculate the mean so far
+
+        hdf5_file["train_img"][i, ...] = img[None]
+        label.append(lbl)
+
+        printProgressBar(i + 1, len(addrs), prefix='Progress:', suffix='Complete', length=50)
+        i = i + 1
+
+label = to_categorical(lbl)
+hdf5_file["train_labels"][i, ...] = label
+
+hdf5_file.close()
